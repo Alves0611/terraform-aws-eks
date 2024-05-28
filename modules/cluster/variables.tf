1616cba8f35ee4f19551df9bd8090311588bb889
Alves0611 <g.alves0611@gmail.com>
2024-05-28 14:51:25 -0300

    feat: define variables for EKS cluster configuration

---
+++ modules/cluster/variables.tf
@@ -0,0 +1,14 @@
+variable "service_name" {
+  type        = string
+  description = "The service name identifier"
+}
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "The service name identifier"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "The service name identifier"
+}
