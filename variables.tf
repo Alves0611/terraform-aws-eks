81ac1883aa78a27e3c662329ac95ee9cc792ef77
Alves0611 <g.alves0611@gmail.com>
2024-05-29 14:14:49 -0300

    refactor: remove unnecessary variables for NAT Gateway creation from root variables.tf

--- variables.tf
+++ variables.tf
@@ -12,13 +12,3 @@ variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
 }
-
-variable "create_nat_gateway" {
-  type        = bool
-  description = "Whether to create NAT Gateway"
-}
-
-variable "nat_gateway_per_az" {
-  type        = bool
-  description = "Whether to create a NAT Gateway in each availability zone"
-}
