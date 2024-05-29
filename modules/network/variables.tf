28c049b55b6ced3c73f797f9ce3b62aed2154ffb
Alves0611 <g.alves0611@gmail.com>
2024-05-29 14:12:24 -0300

    refactor: update variable definitions for VPC module

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,24 +1,18 @@
 variable "aws_region" {
   type        = string
   description = "The region to deploy to"
+  default = "us-east-1"
 }
 
 variable "service_name" {
   type        = string
   description = "The service name identifier"
+  default = "eks"
 }
 
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
+  default = "10.0.0.0/16"
 }
 
-variable "create_nat_gateway" {
-  type        = bool
-  description = "Whether to create NAT Gateway"
-}
-
-variable "nat_gateway_per_az" {
-  type        = bool
-  description = "Whether to create a NAT Gateway in each availability zone"
-}
