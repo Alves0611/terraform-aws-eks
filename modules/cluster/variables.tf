933718f3013e725ab09a0cd067cc4059884e468c
Alves0611 <g.alves0611@gmail.com>
2024-05-29 07:17:45 -0300

    docs: update descriptions for service name and subnet variables

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -1,14 +1,14 @@
 variable "service_name" {
   type        = string
-  description = "The service name identifier"
+  description = "The name of the service for which the EKS cluster is being provisioned"
 }
 
 variable "public_subnet_1a" {
   type        = string
-  description = "The service name identifier"
+  description = "The ID of the public subnet in availability zone 1a"
 }
 
 variable "public_subnet_1b" {
   type        = string
-  description = "The service name identifier"
+  description = "The ID of the public subnet in availability zone 1b"
 }
