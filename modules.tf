54b7fe49d4adb57cbb1f8e69f8641294dcbac0a6
Alves0611 <g.alves0611@gmail.com>
2024-05-29 06:24:56 -0300

    feat: add EKS managed node group module with private subnet configuration

--- modules.tf
+++ modules.tf
@@ -15,3 +15,11 @@ module "eks_cluster" {
   public_subnet_1a = module.eks_network.public_subnet_ids[0]
   public_subnet_1b = module.eks_network.public_subnet_ids[1]
 }
+
+module "eks_managed_node_group" {
+  source            = "./modules/managed-node-group"
+  service_name      = var.service_name
+  cluster_name      = module.eks_cluster.cluster_name
+  subnet_private_1a = module.eks_network.private_subnet_ids[0]
+  subnet_private_1b = module.eks_network.private_subnet_ids[1]
+}
