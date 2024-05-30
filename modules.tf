acefefd40c0810f3aa078c13352a05d7d60cee20
Alves0611 <g.alves0611@gmail.com>
2024-05-29 21:43:20 -0300

    feat: add eks_aws_load_balancer_controller module to modules.tf

--- modules.tf
+++ modules.tf
@@ -1,9 +1,9 @@
 module "eks_network" {
   source = "./modules/network"
 
-  aws_region         = var.aws_region
-  cidr_block         = var.cidr_block
-  service_name       = var.service_name
+  aws_region   = var.aws_region
+  cidr_block   = var.cidr_block
+  service_name = var.service_name
 }
 
 module "eks_cluster" {
@@ -22,4 +22,11 @@ module "eks_managed_node_group" {
   subnet_private_1a = module.eks_network.private_subnet_ids[0]
   subnet_private_1b = module.eks_network.private_subnet_ids[1]
   subnet_private_1c = module.eks_network.private_subnet_ids[2]
+}
+
+module "eks_aws_load_balancer_controller" {
+  source       = "./modules/aws-load-balancer-controller"
+  service_name = var.service_name
+  oidc         = module.eks_cluster.oidc
+  cluster_name = module.eks_cluster.cluster_name
 }
