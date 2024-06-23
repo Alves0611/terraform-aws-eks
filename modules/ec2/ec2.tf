64e2025381ab3b4aeec61122261eb494a0c6a4a6
Alves0611 <g.alves0611@gmail.com>
2024-06-23 18:19:51 -0300

    feat: configure EC2 instances

---
+++ modules/ec2/ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.bastion_ssh.id
+  subnet_id                   = var.public_subnet
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  user_data                   = <<EOF
+#!/bin/bash
+
+apt update
+apt install ansible -y
+EOF
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  key_name               = aws_key_pair.bastion_ssh.id
+  subnet_id              = var.public_subnet
+  vpc_security_group_ids = [aws_security_group.mongodb_sg.id]
+}
+
