fa3b7cac92c7e0dbb005f366dda39f640857e7b6
Alves0611 <g.alves0611@gmail.com>
2024-06-23 18:20:48 -0300

    feat: define module variables

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "service_name" {
+  type        = string
+  description = "The name of the service for which the EKS cluster is being provisioned"
+}
+
+variable "vpc" {
+  type        = string
+  description = "VPC for SG"
+}
+
+variable "public_subnet" {
+  type        = string
+  description = "Subnet for Bastion Host"
+}
+
+variable "private_subnet" {
+  type        = string
+  description = "Subnet for MongoDB"
+}
+
+variable "cluster_sg" {
+  type        = string
+  description = "EKS Cluster Security Group"
+}
