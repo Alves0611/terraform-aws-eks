a47cbfc997e8cc245fc90f6672f01e56262da061
Alves0611 <g.alves0611@gmail.com>
2024-06-26 17:08:41 -0300

    feat: add Helm release for ExternalDNS

--- modules/add-ons/helm.tf
+++ modules/add-ons/helm.tf
@@ -1,3 +1,27 @@
+resource "helm_release" "eks_helm_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.4.7"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
+
+
 resource "helm_release" "sealed_secrets" {
   name       = "sealed-secrets"
   repository = "https://bitnami-labs.github.io/sealed-secrets"
@@ -7,6 +31,30 @@ resource "helm_release" "sealed_secrets" {
 
   set {
     name  = "fullnameOverride"
-    value = "sealed_secrets-controller"
+    value = "sealedsecrets-controller"
+  }
+}
+
+
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  version    = "1.13.0"
+  namespace  = "kube-system"
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
   }
 }
