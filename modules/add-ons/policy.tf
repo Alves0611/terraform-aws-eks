fb9dac11c59638f0e51b1088da487513d5252dfd
Alves0611 <g.alves0611@gmail.com>
2024-06-26 17:11:29 -0300

    chore: define IAM policy for ExternalDNS

--- modules/add-ons/policy.tf
+++ modules/add-ons/policy.tf
@@ -2,3 +2,8 @@ resource "aws_iam_policy" "eks_controller_policy" {
   name   = "${var.service_name}-aws-load-balancer-controller"
   policy = file("${path.module}/iam_policy.json")
 }
+
+resource "aws_iam_policy" "external_dns_policy" {
+  name   = "${var.service_name}-external-dns"
+  policy = file("${path.module}/iam_externaldns_policy.json")
+}
