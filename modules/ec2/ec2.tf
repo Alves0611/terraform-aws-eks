8c0c26f36de086cafb7b5f1ac41c99f185538750
Alves0611 <g.alves0611@gmail.com>
2024-06-26 17:13:43 -0300

    chore: add tags for MongoDB and bastion instances

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
 
   associate_public_ip_address = true
   key_name                    = aws_key_pair.bastion_ssh.id
-  subnet_id                   = var.public_subnet
+  subnet_id                   = var.public_subnet[0]
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   user_data                   = <<EOF
 #!/bin/bash
@@ -12,6 +12,10 @@ resource "aws_instance" "bastion" {
 apt update
 apt install ansible -y
 EOF
+
+  tags = {
+    "Name" : "bastion"
+  }
 }
 
 resource "aws_instance" "mongodb" {
@@ -19,7 +23,11 @@ resource "aws_instance" "mongodb" {
   instance_type = "t3.micro"
 
   key_name               = aws_key_pair.bastion_ssh.id
-  subnet_id              = var.public_subnet
+  subnet_id              = var.private_subnet[0]
   vpc_security_group_ids = [aws_security_group.mongodb_sg.id]
+
+  tags = {
+    "Name" : "mongodb"
+  }
 }
 
